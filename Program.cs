4c576e5eb2b2d83c8e67e390c719c06d1cc51ac9
DenisM <mdr2010@yandex.ru>
2023-02-18 20:54:00 +0500

    add program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] stringArray = new string[4] { "Hello", "2", "world", ":-)" };
+string[] newArray = new string[stringArray.Length];
+int j = 0;
+int size = 3;
+for (int i = 0; i<stringArray.Length; i++)
+{
+    if (stringArray[i].Length <=size)
+    {
+        newArray[j]=stringArray[i];  
+        j++;   
+    }
+}
+Console.Write($"[{String.Join(" ", stringArray)}] -> ");
+Console.Write($"[{String.Join(" ", newArray)}]");
